ad31e8f4e20dc133bcc85641f95c0e3e9bb37fe4
Dmytrii9Cherniak <dcheyrnak10@gmail.com>
2023-07-05 16:59:48 +0300

    fetched data

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -4,7 +4,9 @@ import { getAllProducts } from '../products_service/product.service';
 import { RootState } from '../redux/all_reducers';
 import { ProductActionModel } from '../models/product.action.model';
 import { ThunkDispatch } from 'redux-thunk';
-import {useTypesSelector} from "../hooks/useTypedSelector";
+import { useTypesSelector } from '../hooks/useTypedSelector';
+import { ProductModel } from '../models/product.model';
+import './ProductsList.scss';
 
 function ProductList(): JSX.Element {
     const { products, error, loading } = useTypesSelector(state => state.products);
@@ -15,11 +17,23 @@ function ProductList(): JSX.Element {
     }, []);
 
     useEffect(() => {
-        console.log(products)
+        console.log(products);
     }, [products])
 
-    return (<div>
+    return (<div className="listOfProducts">
+        <header className="header">
 
+        </header>
+        <div className="productList">
+            {products.map((el: ProductModel) =>
+                <div key={el.id} className="productItem">
+                    <img src={el.imageUrl} alt="image" />
+                    <article>
+                        <p> {el.name} </p>
+                    </article>
+                </div>
+            )}
+        </div>
             </div>);
 }
 
--- src/hooks/useTypedSelector.ts
+++ src/hooks/useTypedSelector.ts
@@ -1,5 +1,4 @@
 import { TypedUseSelectorHook, useSelector } from 'react-redux';
 import { RootState } from '../redux/all_reducers';
 
-
 export const useTypesSelector: TypedUseSelectorHook<RootState> = useSelector;
